ef959f0d8193a2866d2c80d76127d5740a8c4d5b
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-23 16:24:23 +0530

    add: Generic Classes to calculate and print the max number

---
+++ GenericMax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+    public class GenericMax<T>
+    {
+        private T[] Values { get; set; }
+
+        public GenericMax(params T[] values)
+        {
+            Values = values;
+            PrintArray();
+        }
+
+        private void PrintArray()
+        {
+            Console.Write($"\nArray type is '{Values.GetType()}' and values are = ");
+            foreach (T item in Values)
+            {
+                Console.Write(item+ ", ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void PrintMax(T variable)
+        {
+            Console.WriteLine("Printing the largest via Generic Class: " + variable);
+        }
+
+        public void TestMaximum()
+        { 
+            T large = Values[0];
+            foreach (T item in Values) 
+            {
+                if(Comparer<T>.Default.Compare(large,item)<0) large = item;
+            }
+            PrintMax(large);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,17 +16,17 @@ namespace GenericClasses
                 //Integer Testcase - 1: Largest among integer numbers
                 IntegerObject integer = new IntegerObject(12, 43, 8);
                 integer.PrintObject();
-                //int largeint = Maximum.MaxOfInput(integer);
-                //integer.Display(largeint);
-                Maximum.TestMaximum(integer.Number);
+                int largeint = Maximum.MaxOfInput(integer);
+                integer.Display(largeint);
+                //Maximum.TestMaximum(integer.Number);
 
 
                 //Integer Testcase - 2: Largest among integer numbers
                 IntegerObject integer1 = new IntegerObject(12, 43, 8, 98, 99, 798, 1890, 0, 76, 576, 45);
                 integer1.PrintObject();
-                //int largeint1 = Maximum.MaxOfInput(integer1);
-                //integer1.Display(largeint1);
-                Maximum.TestMaximum(integer1.Number);
+                int largeint1 = Maximum.MaxOfInput(integer1);
+                integer1.Display(largeint1);
+                //Maximum.TestMaximum(integer1.Number);
 
 
                 //Integer Testcase - 3: Largest among integer numbers
@@ -41,16 +41,16 @@ namespace GenericClasses
                 //Float Testcase - 1: Largest among floating numbers
                 FloatObject floatObject = new FloatObject(0.4f, 0.35f, 0.6f);
                 floatObject.PrintObject();
-                //float largefloat = Maximum.MaxOfInput(floatObject);
-                //floatObject.Display(largefloat);
-                Maximum.TestMaximum(floatObject.Number);
+                float largefloat = Maximum.MaxOfInput(floatObject);
+                floatObject.Display(largefloat);
+                //Maximum.TestMaximum(floatObject.Number);
 
                 //Float Testcase - 2: Largest among floating numbers
                 FloatObject floatObject1 = new FloatObject(0.4f, 0.35f, 0.6f, 0.4589f, 1.23f, -4.78f);
                 floatObject1.PrintObject();
-                //float largefloat1 = Maximum.MaxOfInput(floatObject1);
-                //floatObject1.Display(largefloat1);
-                Maximum.TestMaximum(floatObject1.Number);
+                float largefloat1 = Maximum.MaxOfInput(floatObject1);
+                floatObject1.Display(largefloat1);
+                //Maximum.TestMaximum(floatObject1.Number);
 
                 //Float Testcase - 3: Largest among floating numbers
                 FloatObject floatObject2 = new FloatObject(0.35f);
@@ -62,16 +62,16 @@ namespace GenericClasses
                 //Strings TestCase - 1: Largest among strings
                 StringObject stringObject = new StringObject("Apple", "Peach", "Banana");
                 stringObject.PrintObject();
-                //string largestring = Maximum.MaxOfInput(stringObject);
-                //stringObject.Display(largestring);
-                Maximum.TestMaximum(stringObject.Strings);
+                string largestring = Maximum.MaxOfInput(stringObject);
+                stringObject.Display(largestring);
+                //Maximum.TestMaximum(stringObject.Strings);
 
                 //Strings TestCase - 2: Largest among strings
                 StringObject stringObject1 = new StringObject("A", "B", "C", "D", "Z", "G", "K", "ZZ");
                 stringObject1.PrintObject();
-                //string largestring1 = Maximum.MaxOfInput(stringObject1);
-                //stringObject1.Display(largestring1);
-                Maximum.TestMaximum(stringObject1.Strings);
+                string largestring1 = Maximum.MaxOfInput(stringObject1);
+                stringObject1.Display(largestring1);
+                //Maximum.TestMaximum(stringObject1.Strings);
 
                 //Strings TestCase - 3: Largest among strings
                 StringObject stringObject2 = new StringObject("Arun", "Sujan", "Thanay", "Suprita", "Lalitha");
@@ -94,6 +94,16 @@ namespace GenericClasses
                 StringObject stringObject3 = new StringObject("'1'","'3'","'45'","'99.56'");
                 stringObject3.PrintObject();
                 Maximum.TestMaximum<string>(stringObject3.Strings);
+
+                //Generic Classes
+                GenericMax<int> ints = new GenericMax<int>(12,06,11,09,24);
+                ints.TestMaximum();
+
+                GenericMax<float> floats = new GenericMax<float>(0.5f, 06.89f, 1.1f, 0.9f, 2.4f);
+                floats.TestMaximum();
+
+                GenericMax<string> strings = new GenericMax<string>("hi","am","Thanay","kumar","Y","R");
+                strings.TestMaximum();
 
             }
             catch (Exception ex) 
